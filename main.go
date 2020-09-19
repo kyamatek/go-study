d7dbb46eeee1c1efd0e697a65585864db5b2e5cb
Yamate Kyosuke <ttr.knnb11@ab.auone-net.jp>
2020-09-19 11:41:58 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{})
+	})
+
+	data := "Hello Go/Gin!!"
+
+	router.GET("/test", func(ctx *gin.Context) {
+		ctx.HTML(200, "test.html", gin.H{"data": data})
+	})
+
+	router.Run()
+}
